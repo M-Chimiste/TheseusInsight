96d2e24c87c3d922fd61ed1fbb79f31dbfc511e4
M Chimiste <37678080+M-Chimiste@users.noreply.github.com>
2024-12-31 08:20:40 -0500

    Delete paperpal.sh

--- paperpal.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-source "/Users/c/mambaforge/etc/profile.d/conda.sh"
-conda activate paperpal
-python run_paperpal.py --receiver-address "mark.salacinski@gmail.com, christian.j.merrill@gmail.com"
-conda deactivate
